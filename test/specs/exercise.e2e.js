ce5de5e5d6ef8b7c893ead7c412b1ba5be6cf6e0
Lenka <lenka.liskova@dokamar.cz>
2023-10-25 20:20:24 +0200

    test submition

--- test/specs/exercise.e2e.js
+++ test/specs/exercise.e2e.js
@@ -1,17 +1,154 @@
-import {username, password} from './fixtures.js'
 // import LoginPage from '../pageobjects/login.page'
 // import ApplicationsPage from '../pageobjects/applications.page'
 
+
 describe('Czechitas Login Page', async () => {
 
-    it('should open login page', async () => {
+  it('should login and list applications', async () => {
+
+    const username = 'da-app.admin@czechitas.cz';
+    const password = 'Czechitas123';
 
         await browser.reloadSession();
-
         await browser.url('/prihlaseni');
 
-        await browser.pause(5000);
+        const loginButton = $('.btn-primary');
+        await loginButton.click();
 
+        const emailField = $('#email');
+        const passwordField = $('#password');
+
+
+        await emailField.setValue("nespravneMeno");
+        await passwordField.setValue("nespravneHeslo");
+        await loginButton.click();
+
+        console.log('Prihlásenie úspešné: ' + await emailField.isDisplayed() && await passwordField.isDisplayed());
+
+
+        const UserLogged = await $('=Přihlásit').isDisplayed()
+        console.log("Prihlasenie sa podarilo " + await UserLogged);
+ 
+        await emailField.setValue(username);
+        await passwordField.setValue(password);
+        await loginButton.click();
+
+        console.log('Prihlásenie úspešné: ' + await emailField.isDisplayed() && await passwordField.isDisplayed());
+
+        const currentUser = $('.navbar-right').$('strong').getText()
+        console.log(await currentUser);
+
+        await browser.url('/admin/prihlasky');
+        await browser.pause(1000);
+
+        const rows = await $('.dataTable').$('tbody').$$('tr');
+        console.log('There are ' + rows.length + ' rows in the table');
+        for (const row of rows) {
+            const rowText = await row.getText()
+            //console.log(rowText);
+        }
+
+        await $('input[type="search"]').setValue('bil');
+
+        const userNameDropdown = $('[data-toggle="dropdown"]');
+        userNameDropdown.click()
+        await browser.pause(1000);
+
+
+        const UserLoggedOut = await $('=Odhlásit').isDisplayed()
+        console.log("dropdown displayes: " + UserLoggedOut)
+        await $('=Odhlásit').click()
+        await browser.pause(1000);
     });
 
+});
+
+
+import {username, password} from './fixtures.js'
+
+describe('Login And Applications Page', async () => {
+
+    it('My long and ugly test', async () => {
+        await browser.reloadSession();
+        await browser.url('/prihlaseni');
+
+        // selektory pro elementy na prihlasovacim formulari
+        const emailField = $('#email');
+        const passwordField = $('#password');
+        const loginButton = $('.btn-primary');
+        const fieldError = $('.invalid-feedback');
+        const toastMessage = $('.toast-message');
+
+
+        // uzivatel na je formulari pro prihlaseni
+        console.log('Email field is dislayed: ' + await emailField.isDisplayed());
+        console.log('Password field is dislayed: ' + await passwordField.isDisplayed());
+        console.log('Login button is dislayed: ' + await loginButton.isDisplayed());
+
+
+        // uzivatel nevyplni prihlasovaci udaje a klikne prihlasit - ocekava ze se neprihlasi
+        await loginButton.click();
+        console.log('Email field is dislayed: ' + await emailField.isDisplayed());
+        console.log('Password field is dislayed: ' + await passwordField.isDisplayed());
+        console.log('Login button is dislayed: ' + await loginButton.isDisplayed());
+
+        
+        // uzivatel vyplni spatne prihlasovaci udaje a klikne prihlasit - ocekava ze se neprihlasi
+        await emailField.setValue(username);
+        await passwordField.setValue('invalid');
+        await loginButton.click();
+        console.log('Error: ' + await toastMessage.getText());
+        console.log('Field error: ' + await fieldError.getText());
+        console.log('Email field is dislayed: ' + await emailField.isDisplayed());
+        console.log('Password field is dislayed: ' + await passwordField.isDisplayed());
+        console.log('Login button is dislayed: ' + await loginButton.isDisplayed());
+
+
+        // uzivatel vyplni spravne prihlasovaci udaje a klikne prihlasit - ocekava ze se prihlasi
+        await emailField.setValue(username);
+        await passwordField.setValue(password);
+        await loginButton.click();
+
+        // kontrola jmena prihlaseneho uzivaele
+        const userNameDropdown = $('.navbar-right').$('[data-toggle="dropdown"]');
+        console.log('User currently logged in: ' + await userNameDropdown.getText());
+
+        // prechod na prihlasky
+        await $('=Přihlášky').click();
+        await browser.pause(1000);
+
+        console.log('Page title is: ' + await $('h1').getText());
+
+        // vypis tabulky
+        const rows = await $('.dataTable').$('tbody').$$('tr');
+        console.log('There are ' + rows.length + ' rows in the table');
+        for (const row of rows) {
+            const rowText = await row.getText()
+            console.log(rowText);
+        }
+        
+        // selektory pro filtrovani v tabulce
+        const searchInput = $('input[type="search"]');
+        const loading = $('#DataTables_Table_0_processing');  // !!! zobrazuje sa modal že loading, je tam celý čas ale je displayed none
+        const searchText = 'mar';
+
+        // filtrovani v tabulce
+        await searchInput.setValue(searchText);
+        await browser.pause(1000);
+        await loading.waitForDisplayed({ reverse: true});
+
+        // vypis tabulky
+        const filteredRows = await $('.dataTable').$('tbody').$$('tr')
+        console.log('There are ' + filteredRows.length + ' filtered rows in the table');
+        for (const row of filteredRows) {
+            console.log(await row.getText());
+        }
+
+        // odhlaseni
+        //const navbarRight = $('.navbar-right')
+        const logoutLink = $('#logout-link');
+        await userNameDropdown.click();
+        await logoutLink.click();
+        console.log('Navbar text: ' + await navbarRight.getText());
+    });
 });
--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -1,9 +1,39 @@
-describe('Homework', async () => {
+describe("Homework", async () => {
+  // HW1
+  it("should open login page", async () => {
+    await browser.reloadSession();
+    await browser.url("/prihlaseni");
+    await browser.saveScreenshot("screenshots/login_page.png");
+    await browser.pause(5000);
+  });
 
-    it('should open page and create screenshot', async () => {
+  // HW2
+  it("examples of selectors", async () => {
+    // $('#email')            // Políčko pro email
+    // $('#name')             // Políčko pro jméno a příjmení - ale v registraci
+    // $('#password')         // Políčko pro zadání hesla
+    // $('#password-confirm') // Políčko pro kontrolu zadaného hesla
+    // $('.btn-secondary')    // Tlačítko na registraci
+    //console.log(await $('.btn-secondary').getText())
+  });
 
-        // sem vypracuj domácí úkol
+  // HW3
+  it("should register new user", async () => {
+    await browser.reloadSession();
+    await browser.url("/registrace");
 
-    });
+    const nameField = $("#name");
+    const emailField = $("#email");
+    const passwordField = $("#password");
+    const passwordConfirmField = $("#password-confirm");
+    const loginButton = $(".btn-primary");
+    //const fieldError = $('.invalid-feedback');
+    //const toastMessage = $('.toast-message');
 
+    await nameField.setValue("Name Surname");
+    await emailField.setValue("name.surname@gmail.com");
+    await passwordField.setValue("password");
+    await passwordConfirmField.setValue("password");
+    await loginButton.click();
+  });
 });
